c6f373412b77540789da60284ad7475d8f3a2bb0
freddy <ffernacu@hotmail.com>
2025-10-14 22:36:06 -0500

    impl discovery client

--- src/main/java/pe/ffernacu/spring_reactor_academy/SpringReactorAcademyApplication.java
+++ src/main/java/pe/ffernacu/spring_reactor_academy/SpringReactorAcademyApplication.java
@@ -2,7 +2,9 @@ package pe.ffernacu.spring_reactor_academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringReactorAcademyApplication {
 
